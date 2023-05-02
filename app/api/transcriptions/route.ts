3bc7ac14fc511400da533f7531988823a1465bf8
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2023-05-02 21:36:13 +0100

    chore: update signatures, enable CORS

--- app/api/transcriptions/route.ts
+++ app/api/transcriptions/route.ts
@@ -1,6 +1,18 @@
 import { NextResponse } from "next/server";
 import { YoutubeTranscript } from "youtube-transcript";
 
+type YTTranscriptLine = {
+  text: string;
+  offset: number;
+  duration: number;
+};
+
+type ResponseTranscriptLine = {
+  text: string;
+  offsetSeconds: number;
+  durationSeconds: number;
+};
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("id");
@@ -9,11 +21,30 @@ export async function GET(request: Request) {
     return new Response("Missing id", { status: 400 });
   }
 
-  const transcript = await YoutubeTranscript.fetchTranscript(id);
+  const transcript = await YoutubeTranscript.fetchTranscript(id).then(
+    (lines: YTTranscriptLine[]) =>
+      lines.map(
+        ({ duration, offset, text }) =>
+          ({
+            text,
+            offsetSeconds: offset / 1000,
+            durationSeconds: duration / 1000,
+          } as ResponseTranscriptLine)
+      )
+  );
 
   if (!transcript) {
     return new Response("Cannot get the transcript", { status: 500 });
   }
 
-  return NextResponse.json({ transcript });
+  return NextResponse.json(
+    { transcript },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    }
+  );
 }
